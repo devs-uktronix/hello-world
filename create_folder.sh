c855b00f1c3b02c1f129558a9539d5d71b2ba671
devs-uktronix <redwan@redwans-MacBook-Pro.local>
2021-10-07 14:56:54 +0100

    more test

--- create_folder.sh
+++ create_folder.sh
@@ -6,4 +6,4 @@ TODAY=$(date +%Y-%m-%d)
 mkdir -p ${DST_WORK_DIR}_${TODAY}
 
 
-#rsync -e "ssh -i /var/lib/jenkins/workspace/Hello_World_Pipeline_add-tests/id_ed25519" -Paz redwan@10.10.10.11:/home/redwan/fake_ssl/test_certs_import .
+rsync -e "ssh -i /var/lib/jenkins/workspace/Hello_World_Pipeline_add-tests/id_ed25519" -Paz redwan@10.10.10.11:/home/redwan/fake_ssl/test_certs_import ${DST_WORK_DIR}_${TODAY}
