620fbd0c61ca03fa0bf25874578d3fde6c24f6de
devs-uktronix <redwan@redwans-MacBook-Pro.local>
2021-10-07 14:49:10 +0100

    more test

--- create_folder.sh
+++ create_folder.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/sh
 
-mkdir -p temp_ssl_backup_`date +%F`
+DST_WORK_DIR='temp_ssl_backup_`date +%F`'
+
+
+mkdir -p ${DST_WORK_DIR}
+
+
+#rsync -e "ssh -i /var/lib/jenkins/workspace/Hello_World_Pipeline_add-tests/id_ed25519" -Paz redwan@10.10.10.11:/home/redwan/fake_ssl/test_certs_import .
