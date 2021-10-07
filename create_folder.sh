f59527a12f8b9e6e48acd5d6ea742a36c9ec259c
devs-uktronix <redwan@redwans-MacBook-Pro.local>
2021-10-07 15:12:22 +0100

    more test

--- create_folder.sh
+++ create_folder.sh
@@ -5,10 +5,10 @@ TODAY=$(date +%Y-%m-%d)
 SSH_KEY='/var/lib/jenkins/workspace/Hello_World_Pipeline_add-tests/id_ed25519'
 SRC_CERT_HOSTNAME='10.10.10.11'
 SRC_CERT_HOSTNAME_PATH='/home/redwan/fake_ssl/test_certs_import'
-
+USER='redwan'
 DST_CERT_DIR=${DST_WORK_DIR}_${TODAY}
 
 mkdir -p ${DST_CERT_DIR}
 
 
-rsync -e "ssh -i ${SSH_KEY}" -az redwan@${SRC_CERT_HOSTNAME}:${SRC_CERT_HOSTNAME_PATH} ${DST_CERT_DIR}
+rsync -e "ssh -i ${SSH_KEY}" -az ${USER}@${SRC_CERT_HOSTNAME}:${SRC_CERT_HOSTNAME_PATH} ${DST_CERT_DIR}
